a7d7c7e9736f1d07bb7efc9b0232d2f519cbb2d8
age401 <age401zzz@gmail.com>
2018-07-16 10:15:59 -0300

    Created PlayerHandler script, with movement and jump based on Character Controller Unity Scripting reference.

---
+++ Assets/_Scripts/PlayerHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour {
+
+    // Movement Levers
+    [SerializeField] float defSpeed = 10;
+    [SerializeField] float defJumpSpeed = 8;
+    [SerializeField] float defGravity = 1;
+    // Movement
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Components
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= defSpeed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = defJumpSpeed;
+
+        }
+        else
+        {
+            moveDirection.y -= defGravity * Time.deltaTime;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
