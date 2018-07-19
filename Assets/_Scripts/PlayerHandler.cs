14c9ace06e09f9bb63a0a99d8e5fc49431726b89
age401 <age401zzz@gmail.com>
2018-07-19 02:24:56 -0300

    Movement System reworked -Again!-

--- Assets/_Scripts/PlayerHandler.cs
+++ Assets/_Scripts/PlayerHandler.cs
@@ -5,38 +5,53 @@ using UnityEngine;
 public class PlayerHandler : MonoBehaviour {
 
     // Movement Levers
-    [SerializeField] float defSpeed = 10;
-    [SerializeField] float defJumpSpeed = 8;
-    [SerializeField] float defGravity = 1;
+    public float defSpeed = 10;
+    public float defJumpSpeed = 8;
+    public float defGravity = 1;
     // Movement
+    private float moveHorizontal;
+    private float moveVertical;
+    private Vector3 faceDirection = Vector3.zero;
     private Vector3 moveDirection = Vector3.zero;
 
     // Components
     CharacterController controller;
+    Animator anim;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
+        movePlayer();
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= defSpeed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = defJumpSpeed;
-
         }
         else
         {
             moveDirection.y -= defGravity * Time.deltaTime;
         }
+    }
 
-        controller.Move(moveDirection * Time.deltaTime);
+    void movePlayer()
+    {
+        if (controller.isGrounded)
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            moveVertical = Input.GetAxis("Vertical");
+            moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+            if (moveDirection.magnitude > 0.01f)
+            {
+                this.transform.forward = moveDirection;
+            }
+        }
 
+        controller.Move(moveDirection * defSpeed * Time.deltaTime);
     }
 }
