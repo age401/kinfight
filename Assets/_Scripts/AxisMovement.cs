6faceca2e6b0d08d6fc9267f9e450ab73b4f48ef
Diego Martinez <age401zzz@gmail.com>
2018-07-28 14:16:32 -0300

    Restored commits after diff errors.

---
+++ Assets/_Scripts/AxisMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisMovement : MonoBehaviour 
+{
+	public float speed;
+	public float rotSpeed;
+
+	public void Update()
+	{
+		Vector3 movFwd = transform.forward * Input.GetAxis ("Vertical");
+		Vector3 movRight = transform.right * Input.GetAxis ("Horizontal");
+		Vector3 totalMov = (movFwd + movRight) * speed;
+		transform.position += totalMov * Time.deltaTime;
+
+		float rotY = Input.GetAxis ("Mouse X") * rotSpeed * Time.deltaTime;
+		transform.Rotate (0, rotY, 0);
+	}
+}
